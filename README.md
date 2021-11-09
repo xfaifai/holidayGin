2c441d42c7eb1e9e17b9706fb05363956eb8d558
特斯拉是神 <boy-alien@qq.com>
2021-11-09 14:45:23 +0800

    完善功能

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # holidayGin
 Get the day is holiday or not.
+
+[Only support Chinese Mainland.]
+
+- ## Usage  
+Running (default port 8080)
+````
+git clone https://github.com/xfaifai/holidayGin.git
+
+go mod tidy
+
+go run main.go
+````
+
+Request
+````
+http://localhost:8080/YYYYmmdd 
+
+YYYYmmdd is which day you need.
+
+Example:
+http://localhost:8080/20220101 
+````
+
+Response
+````
+{
+    "data": {
+        "20220104": 0
+    },
+    "status": "success"
+}
+
+Type:
+0 Working day.
+1 Double rest day
+2 Triple rest day
+````
+
+
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"os"
-	"strconv"
 	"time"
 )
 
@@ -16,50 +15,38 @@ func main() {
 		date := c.Param("date")
 		t,err := time.Parse("20060102", date)
 		if err != nil {
-			c.JSON(200,gin.H{
-				"status":"error",
-				"message":"时间格式不对",
-			})
+			c.JSON(200,gin.H{"status":"error", "message":"时间格式不对"})
 			return
 		}
 
-		year,_ := strconv.Atoi(t.Format("2006"))
-		if year < 2010 {
-			c.JSON(200,gin.H{
-				"status":"error",
-				"message":"暂未支持的年份",
-			})
-			return
-		}
-
-		jsonPath := "./data/"+strconv.Itoa(year)+".json"
+		year := t.Format("2006")
+		jsonPath := "./data/" + year + ".json"
 		jsonFile,err := os.Open(jsonPath)
 		if err != nil {
-			c.JSON(200,gin.H{
-				"status":"error",
-				"message":"暂未支持的年份",
-			})
+			c.JSON(200,gin.H{"status":"error", "message":"暂未支持的年份"})
 			return
 		}
 
 		defer jsonFile.Close()
 
 		byteValue,_ := ioutil.ReadAll(jsonFile)
-
 		var jsonData map[string]interface{}
 		json.Unmarshal([]byte(byteValue),&jsonData)
 
 		day := t.Format("0102")
 		value,ok := jsonData[day]
 		if ok {
-			c.JSON(200,gin.H{
-				"status":"success",
-				"data": gin.H{
-					date:value,
-				},
-			})
+			c.JSON(200,gin.H{"status":"success", "data": gin.H{date:value}})
 			return
 		}
+
+		w := int(t.Weekday())
+		if w==6 || w==0 {
+			c.JSON(200,gin.H{"status":"success", "data": gin.H{date:1}})
+		} else {
+			c.JSON(200,gin.H{"status":"success", "data": gin.H{date:0}})
+		}
 	})
-	r.Run()
+
+	r.Run(":8080")
 }
